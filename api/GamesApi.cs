ef58166a98492f6def4e5c79ee5c3033d2dadaca
Odenstig <erik.odenstig@live.se>
2022-11-03 17:33:55 +0100

    Pushu

--- api/GamesApi.cs
+++ api/GamesApi.cs
@@ -1,13 +1,11 @@
-using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using Microsoft.Azure.Cosmos.Table;
+using System.Linq;
 
 namespace functions
 {
@@ -21,17 +19,29 @@ namespace functions
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            var query = table.CreateQuery<TableData>();
+            query.TakeCount = 1;
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            var games = (await table.ExecuteQuerySegmentedAsync(query, null)).ToList();
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            if (games.Any())
+            {
+                var result = new ApiResponse(
+                    games.First().Json
+                );
 
-            return new OkObjectResult(responseMessage);
+                log.LogInformation(result.Json);
+
+                return new OkObjectResult(result);
+            }
+
+            return new OkResult();
         }
     }
+    public record ApiResponse(string Json);
+    public class TableData : TableEntity
+    {
+        public string Json { get; set; }
+    }
+
 }
